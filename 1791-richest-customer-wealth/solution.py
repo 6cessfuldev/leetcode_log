81bcfbc1dda3f8982aeae5c08af5976529642890
Yuk seongmin <89137580+6cessfuldev@users.noreply.github.com>
2025-07-31 14:14:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (54.20%)

---
+++ 1791-richest-customer-wealth/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        maxSum = 0
+
+        for i in accounts:
+            maxSum = max(maxSum, sum(i))
+
+        return maxSum
+        
